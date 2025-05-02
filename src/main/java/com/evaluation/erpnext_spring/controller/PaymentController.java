8ae0510c000717d68a80984cb3eb4750ad868896
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-05-02 22:55:17 +0300

    ok

---
+++ src/main/java/com/evaluation/erpnext_spring/controller/PaymentController.java
@@ -0,0 +1,10 @@
+package com.evaluation.erpnext_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/payments")
+public class PaymentController {
+    
+}
