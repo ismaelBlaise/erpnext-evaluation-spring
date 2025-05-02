fb0168e6e3ca333d56f8c7c9f7891ec718909455
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-05-02 23:41:43 +0300

    Update PaymentController.java

--- src/main/java/com/evaluation/erpnext_spring/controller/PaymentController.java
+++ src/main/java/com/evaluation/erpnext_spring/controller/PaymentController.java
@@ -1,18 +1,28 @@
 package com.evaluation.erpnext_spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.evaluation.erpnext_spring.service.PaymentService;
+
 @Controller
 @RequestMapping("/payments")
 public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
     @GetMapping
     public ModelAndView paymentForm(@RequestParam String facture){
         ModelAndView modelAndView=new ModelAndView("template");
+        
         modelAndView.addObject("page","invoices/payment");
         return modelAndView;
     }
+
+
 }
